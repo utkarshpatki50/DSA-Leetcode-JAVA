203d6a863a3a27862f782ee18aee3c20eb48f42c
Utkarsh Patki <utkarshpatki50@gmail.com>
2025-04-04 20:53:26 +0530

    Time: 5 ms (97.76%), Space: 44.6 MB (67.1%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,30 +1,31 @@
 class Solution {
 
     private boolean isOperator(String ch) {
-        return ch.equals("+") || ch.equals("-") || ch.equals("*") || ch.equals("/");
+        if (ch.equals("+") || ch.equals("-") || ch.equals("*") || ch.equals("/"))
+            return true;
+        return false;
     }
 
     public int evalRPN(String[] tokens) {
         int n = tokens.length;
         Stack<Integer> st = new Stack<>();
+        int ans = 0;
+
         for (int i = 0; i < n; i++) {
             String ch = tokens[i];
-
-            if (isOperator(ch)) {
-                int ans = 0;
-                int b = st.pop();
+            if (isOperator(ch) == true) {
                 int a = st.pop();
+                int b = st.pop();
 
                 if (ch.equals("+")) {
                     ans = a + b;
                 } else if (ch.equals("-")) {
-                    ans = a - b;
+                    ans = b - a;
                 } else if (ch.equals("*")) {
                     ans = a * b;
-                } else if (ch.equals("/")) {
-                    ans = a / b;
+                } else {
+                    ans = b / a;
                 }
-
                 st.push(ans);
             } else {
                 st.push(Integer.parseInt(ch));
