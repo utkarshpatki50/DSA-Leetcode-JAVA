b677acd8e3bbc6dbea500fbc6739dd0298cc593b
Utkarsh Patki <utkarshpatki50@gmail.com>
2025-10-02 16:48:44 +0530

    Time: 0 ms (100%), Space: 42.2 MB (31.73%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,33 +1,25 @@
 class Solution {
 
-    // General Rule:
-    // For nums[mid] == 2: Don’t increment mid after swapping, because the swapped
-    // value might not be in its correct position and requires re-evaluation.
-    // For nums[mid] == 0 or 1: You always increment mid, as their placement is
-    // final after processing.
-
-    //DUTCH-NATIONAL FLAG ALGORITHM
-
-    public void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
+    public void swap(int [] nums, int i, int j){
+        int temp= nums[i];
         nums[i] = nums[j];
-        nums[j] = temp;
+        nums[j]= temp;
     }
 
     public void sortColors(int[] nums) {
-        int n = nums.length;
-        int low = 0, mid = 0, high = n - 1;
+        int n= nums.length;
+        int low=0, mid=0, high= n-1;
 
-        while (mid <= high) {
-            if (nums[mid] == 0) {
+        while(mid <= high){
+            if(nums[mid]==0){
                 swap(nums, mid, low);
                 low++;
                 mid++;
-            } else if (nums[mid] == 1) {
+            }
+            else if(nums[mid] ==1 ){
                 mid++;
             }
-
-            else {
+            else{
                 swap(nums, mid, high);
                 high--;
             }
