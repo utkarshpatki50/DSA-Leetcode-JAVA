1bda152b296c03c93516ddceae9abc8cf9e11ca3
Utkarsh Patki <utkarshpatki50@gmail.com>
2025-10-08 19:38:44 +0530

    Time: 0 ms (100%), Space: 42.1 MB (69.64%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -6,23 +6,25 @@ class Solution {
         while (low <= high) {
             int mid = low + (high - low) / 2;
 
-            if (nums[mid] == target)
+            if (nums[mid] == target) {
                 return mid;
+            }
 
             if (nums[low] <= nums[mid]) {
+                //left half sorted
                 if (target >= nums[low] && target < nums[mid]) {
                     high = mid - 1;
                 } else {
                     low = mid + 1;
                 }
             } else {
+                //right half sorted
                 if (target > nums[mid] && target <= nums[high]) {
                     low = mid + 1;
                 } else {
                     high = mid - 1;
                 }
             }
-
         }
         return -1;
     }
