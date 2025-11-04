df18c7d347a3412fd4f4644a1f030122cc748748
Utkarsh Patki <utkarshpatki50@gmail.com>
2025-11-05 00:26:16 +0530

    Time: 5 ms (63.96%), Space: 82.9 MB (5.16%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -4,8 +4,8 @@ class Solution {
         int slow = nums[0], fast = nums[0];
 
         do {
-            slow = nums[slow];   //move by one
-            fast = nums[nums[fast]];   //move by two
+            slow = nums[slow];
+            fast = nums[nums[fast]];
 
         } while (slow != fast);
 
